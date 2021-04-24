ef5a4162e594a466f1761536099ed2587fb1c358
Zhermit09 <71440273+Zhermit09@users.noreply.github.com>
2021-04-24 15:01:24 +0200

    Successfully Fetched assignments

--- script.js
+++ script.js
@@ -4,25 +4,22 @@ window.onload = () => urlElementsCheck();
 let token;
 
 function urlElementsCheck() {
-
-    try {
-        const url = new URL(window.location.href);
-        if (url.hash === null || url.hash === "") {
+    token = JSON.parse(sessionStorage.getItem("Token"));
+    if (token === null) {
+        sessionStorage.setItem("Token", JSON.stringify(new URLSearchParams(window.location.hash).get('access_token')));
+        if (token === null) {
             console.log("You don't have the token")
             OAuth20();
-        } else {
-            console.log("You have the token");
-            //maybe later do sessionStorage.setItem()
-            token = new URLSearchParams(window.location.hash).get('access_token');
         }
-    } catch (error) {
-        console.error();
+    } else {
+        console.log("You have the token");
     }
+   // location.hash = "";
 }
 
 function OAuth20() {
     window.location.replace("https://accounts.google.com/o/oauth2/v2/auth?" +
-        "scope=https://www.googleapis.com/auth/classroom.courses&" +
+        "scope=https://www.googleapis.com/auth/classroom.coursework.me.readonly&" +
         "include_granted_scopes=true&" +
         "response_type=token&" +
         "state=state_parameter_passthrough_value&" +
@@ -30,30 +27,34 @@ function OAuth20() {
         "client_id=82346440292-hlpvrpvqk6epjgqkk93566mdd6mtqocp.apps.googleusercontent.com");
 }
 
-
+let DATA;
 
 async function Fetch() {
 
+    if (DATA === undefined) console.log("Empty");
+    token = JSON.parse(sessionStorage.getItem("Token"));
     let h = new Headers();
     h.append('Authorization', `Bearer ${token}`);
 
-    let req = new Request('https://classroom.googleapis.com/v1/courses', {
+    let req = new Request('https://classroom.googleapis.com/v1/courses/129933204397/courseWork', {
         method: 'GET',
         headers: h
     })
     try {
+
         const response = await fetch(req);
-        const {courses} = await response.json();
-        const {0:{enrollmentCode}} = courses;
-        return enrollmentCode;
-    }catch (error){
+        const t = await response.json();
+        DATA = t;
+        if (DATA !== null) console.log("Fetched");
+        console.log(DATA);
+    } catch (error) {
         console.error(error);
     }
 }
-let DATA;
-async function Log() {
- DATA = await Fetch();
-console.log(DATA);
+
+function Log() {
+
+    OAuth20();
 }
 
 /*console.log("banana");
