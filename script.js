436d19534685c493ddd44796a1dfc6d159c1ea65
Zhermit09 <71440273+Zhermit09@users.noreply.github.com>
2021-04-30 22:33:12 +0200

    Batch requests, got assignments status and more or less cleaned the code

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 "use strict";
 window.onload = () => urlElementsCheck();
 let token;
-let classID = [];
+let header;
 
+let scope = "https://www.googleapis.com/auth/classroom.coursework.me.readonly&" +
+    "https://www.googleapis.com/auth/classroom.coursework.students.readonly&";
+
+let assignments;
+let DATA;
 
 function urlElementsCheck() {
     token = JSON.parse(sessionStorage.getItem("Token"));
     if (token === null) {
         sessionStorage.setItem("Token", JSON.stringify(new URLSearchParams(window.location.hash).get('access_token')));
+        token = JSON.parse(sessionStorage.getItem("Token"));
         if (token === null) {
             console.log("You don't have the token")
             OAuth20();
@@ -16,12 +22,12 @@ function urlElementsCheck() {
     } else {
         console.log("You have the token");
     }
-    // location.hash = "";
+    location.hash = "";
 }
 
 function OAuth20() {
     window.location.replace("https://accounts.google.com/o/oauth2/v2/auth?" +
-        "scope=https://www.googleapis.com/auth/classroom.coursework.me.readonly&" +
+        `scope=${scope}` +
         "include_granted_scopes=true&" +
         "response_type=token&" +
         "state=state_parameter_passthrough_value&" +
@@ -29,42 +35,74 @@ function OAuth20() {
         "client_id=82346440292-hlpvrpvqk6epjgqkk93566mdd6mtqocp.apps.googleusercontent.com");
 }
 
-let DATA;
 
 async function Fetch() {
-
-    if (DATA === undefined) console.log("Empty");
     token = JSON.parse(sessionStorage.getItem("Token"));
-    let h = new Headers();
-    h.append('Authorization', `Bearer ${token}`);
-
-    //  /129933204397/courseWork
-    let req = new Request('https://classroom.googleapis.com/v1/courses', {
-        method: 'GET',
-        headers: h
-    })
+    header = new Headers();
+    header.append('Authorization', `Bearer ${token}`);
+    DATA = [];
     try {
-        const response = await fetch(req);
+        const response = await fetch('https://classroom.googleapis.com/v1/courses', {
+            method: 'GET',
+            headers: header
+        });
         DATA = await response.json();
-        if (DATA !== null) console.log("Fetched");
         console.log(DATA);
     } catch (error) {
         console.error(error);
     }
+    await assigmFetch();
 }
 
-async function Assignments() {
-    let h = new Headers();
-    h.append('Authorization', `Bearer ${token}`);
+let BATCH;
 
-    for (const element in DATA.courses) {
-        let clas = DATA.courses[element].id.toString();
-        let response = await fetch(`https://classroom.googleapis.com/v1/courses/${clas}/courseWork`,{
-            method: 'GET',
-            headers: h
+
+async function assigmFetch() {
+    let class_id = "";
+    BATCH = [];
+    const {courses} = DATA
+    try {
+        courses.forEach((assignment) => {
+            class_id = assignment.id.toString();
+            BATCH.push(fetch(`https://classroom.googleapis.com/v1/courses/${class_id}/courseWork`, {
+                method: 'GET',
+                headers: header
+            }).then((res) => res.json()))
         })
-        let assigment = await response.json();
-        console.log(assigment)
+        assignments = await Promise.all(BATCH);
+    } catch (error) {
+        console.error(error);
     }
-   // console.log(classID)
+    console.log(assignments)
+    await submFetch();
 }
+
+let assBatch;
+let submissions;
+
+async function submFetch() {
+    assBatch = [];
+
+    let assigment_id;
+    let class_id;
+
+    assignments.forEach((ass) => {
+        const {courseWork} = ass;
+        for (const index in courseWork)
+            if (courseWork[index].dueDate !== undefined) {
+
+                assigment_id = courseWork[index].id
+                class_id = courseWork[index].courseId
+
+                assBatch.push(fetch(`https://classroom.googleapis.com/v1/courses/${class_id}/courseWork/${assigment_id}/studentSubmissions`, {
+                    method: 'GET',
+                    headers: header
+                }).then((res) => res.json()))
+                //               .then((data) => console.log(data)))
+            }
+    })
+    submissions = await Promise.all(assBatch);
+    console.log(submissions)
+
+}
+
