0f02e90375ba3f779fa6674149a24cfd44c6b286
Zhermit09 <71440273+Zhermit09@users.noreply.github.com>
2021-05-07 11:45:31 +0200

    Calendar magic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //@ts-check
 "use strict";
-window.onload = () => urlElementsCheck();
+window.onload = () => calendar();
 let token;
 let header;
 
@@ -23,6 +23,7 @@ function urlElementsCheck() {
         console.log("You have the token");
     }
     location.hash = "";
+    calendar();
 }
 
 function OAuth20() {
@@ -36,7 +37,7 @@ function OAuth20() {
 }
 
 
-async function Fetch() {
+async function courseFetch() {
     token = JSON.parse(sessionStorage.getItem("Token"));
     header = new Headers();
     header.append('Authorization', `Bearer ${token}`);
@@ -51,13 +52,13 @@ async function Fetch() {
     } catch (error) {
         console.error(error);
     }
-    await assigmFetch();
+    await assigmentFetch();
 }
 
 let BATCH;
 
 
-async function assigmFetch() {
+async function assigmentFetch() {
     let class_id = "";
     BATCH = [];
     const {courses} = DATA
@@ -74,13 +75,13 @@ async function assigmFetch() {
         console.error(error);
     }
     console.log(assignments)
-    await submFetch();
+    await statusFetch();
 }
 
 let assBatch;
 let submissions;
 
-async function submFetch() {
+async function statusFetch() {
     assBatch = [];
 
     let assigment_id;
@@ -106,3 +107,61 @@ async function submFetch() {
 
 }
 
+const date = new Date();
+
+function calendar() {
+    const calMonth = document.querySelector(".date h1")
+    const calFullDate = document.querySelector(".date p")
+    const calDays = document.querySelector('.days')
+
+    let days = "";
+
+
+    const months = [
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"
+    ]
+    const monthLDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    const lMonthLDay = new Date(date.getFullYear(), date.getMonth(), 0);
+    const prevDate = (lMonthLDay.getDate() - (lMonthLDay.getDay() - 1));
+    let nextDate = 7 - monthLDay.getDay();
+
+
+    calMonth.innerHTML = months[date.getMonth()]
+    calFullDate.innerHTML = date.toDateString()
+    for (let k = prevDate; k <= lMonthLDay.getDate(); k++) {
+        days += `<div class="prev-date">${k}</div>`;
+        calDays.innerHTML = days;
+    }
+    for (let i = 1; i <= monthLDay.getDate(); i++) {
+        if (i === date.getDate() && date.getMonth() === new Date().getMonth() && new Date().getFullYear()===date.getFullYear()) {
+            days += `<div class="today">${i}</div>`;
+        }else if (i === date.getDate() && date.getMonth() === new Date().getMonth()){
+            days += `<div class="otherToday">${i}</div>`;
+        }
+        else {
+            days += `<div>${i}</div>`;
+            calDays.innerHTML = days;
+        }
+    }
+
+    if (lMonthLDay.getDay() + monthLDay.getDate() < 35) nextDate += 7;
+    for (let j = 1; j <= nextDate; j++) {
+        days += `<div class="prev-date">${j}</div>`;
+        calDays.innerHTML = days;
+    }
+}
+
+document.querySelector(".prev")
+    .addEventListener("click", () => {
+        date.setMonth(date.getMonth() - 1)
+        calendar()
+    })
+document.querySelector(".next")
+    .addEventListener("click", () => {
+        date.setMonth(date.getMonth() + 1)
+        console.log(date.getFullYear())
+        calendar()
+    })
