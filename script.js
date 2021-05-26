97967c5253f787764940c0b518eb5faa0bf08549
Zhermit09 <71440273+Zhermit09@users.noreply.github.com>
2021-05-26 19:18:38 +0200

    url

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const OAuth20 =
     "include_granted_scopes=true&" +
     "response_type=token&" +
     "state=state_parameter_passthrough_value&" +
-    "redirect_uri=http://localhost:63342/WebApp/index.html&" +
+    "redirect_uri=https://zhermit09.github.io/WebApp/&" +
     "client_id=82346440292-hlpvrpvqk6epjgqkk93566mdd6mtqocp.apps.googleusercontent.com";
 //http://localhost:63342/WebApp/index.html https://zhermit09.github.io/WebApp/
 let header = new Headers()
